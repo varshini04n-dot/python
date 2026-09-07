e7c9f3e0a0b274c3250009cf1a610f40668f6ecd
Varshini <varshini04.n@gmail.com>
2026-09-07 18:51:37 +0530

    juice shop

---
+++ cw1.py
@@ -0,0 +1,13 @@
+apple=15.5
+orange=20
+grape=10.25
+total_volume=apple+orange+grape
+print(total_volume)
+total_int=int(total_volume)
+print(total_int)
+total_string=str(total_volume)
+print("The string of the integer"+"="+total_string)
+import random
+additionalbonus=random.randrange(5,10)
+final=total_volume+additionalbonus
+print(final)
