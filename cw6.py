9f138ec73b71c3c7ad421dfab7149c81001523bd
Varshini <varshini04.n@gmail.com>
2026-09-15 10:38:58 +0530

    attendance tracking

---
+++ cw6.py
@@ -0,0 +1,16 @@
+
+full=0
+total=0
+attendance = [18, 20, 19, 15, 21]
+for x in attendance:
+    if x>=20:
+        print("Full")
+        full+=1
+    else:
+        print("Not full")
+    total+=x
+print(full)
+print(total)
+
+        
+       
